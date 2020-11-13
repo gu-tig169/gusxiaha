67d17b525acf9b5258163dba10688378e0f6df73
WilliamForGit <haiou.xiao@gmail.com>
2020-11-13 17:00:17 +0100

    commit steg 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:todosmart/pages/home.dart';
+import 'package:todosmart/pages/addpage.dart';
 
+void main() {
+  runApp(Nav2App());
+}
+
+class Nav2App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: (settings) {
+        // Handle '/'
+        if (settings.name == '/') {
+          return MaterialPageRoute(builder: (context) => HomeScreen());
+        }
+
+        // Handle '/details/'
+        var uri = Uri.parse(settings.name);
+        if (uri.pathSegments.length == 2 &&
+            uri.pathSegments.first == 'details') {
+          return MaterialPageRoute(builder: (context) => AddScreen());
+        }
+
+        return MaterialPageRoute(builder: (context) => UnknownScreen());
+      },
+    );
+  }
+}
+
+class UnknownScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('404!'),
+      ),
+    );
+  }
+}
